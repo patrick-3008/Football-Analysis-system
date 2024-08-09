2264b0b772481cb80bb967e109a0e14575e03514
Patrick <patricknaashat@yahoo.com>
2024-08-09 11:57:48 +0300

    create new files

---
+++ utils/video_utils.py
@@ -0,0 +1,11 @@
+import cv2
+
+def read_video(video_path):
+    cap = cv2.VideoCapture(video_path)
+    frames = []
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frames.append(frame)
+    return frames
