780998300996b18d317b34b8cbbc1897c03236e5
Patrick <patricknaashat@yahoo.com>
2024-08-09 12:09:43 +0300

    rename input video

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from utils import read_video, save_video
 
 def main():
     # Read Video
-    video_frames = read_video('input_videos/08fd33_4.mp4')
+    video_frames = read_video('input_videos/input_video.mp4')
 
     # Save Video
     save_video(video_frames, 'output_videos/output_video.avi')
--- yolo_inference.py
+++ yolo_inference.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 model = YOLO('models/best.pt')
 
-results = model.predict('input_videos/08fd33_4.mp4', save=True)
+results = model.predict('input_videos/input_video.mp4', save=True)
 print(results[0])
 print('=====================================')
 for result in results:
