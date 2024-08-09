148f882507c3a500ec3a2efece2325feee3fb4ca
Patrick <patricknaashat@yahoo.com>
2024-08-09 12:48:21 +0300

    convert goalkeeper to player

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from utils import read_video, save_video
+from trackers import Tracker
 
 def main():
     # Read Video
     video_frames = read_video('input_videos/input_video.mp4')
+
+    # Init Tracker
+    tracker = Tracker('models/best.pt')
+    tracks = tracker.get_object_tracks(video_frames)
 
     # Save Video
     save_video(video_frames, 'output_videos/output_video.avi')
--- trackers/tracker.py
+++ trackers/tracker.py
@@ -1,4 +1,5 @@
 from ultralytics import YOLO
+import supervision as sv
 
 class Tracker:
     def __init__(self, model_path) -> None:
@@ -10,7 +11,25 @@ class Tracker:
         for i in range(0,len(frames),batch_size):
             detections_batch = self.model.predict(frames[i:i+batch_size],conf=0.1)
             detections += detections_batch
+            break
         return detections
 
     def get_object_tracks(self, frames):
         detections = self.detect_frames(frames)
+
+        # Remove goalkeeper class
+        for frame_num, detection in enumerate(detections):
+            cls_names = detection.names
+            cls_names_inv = {v:k for k,v in cls_names.items()}
+
+            # Convert to supervision Detection format
+            detection_supervision = sv.Detections.from_ultralytics(detection)
+
+            # Convert goalkeeper to player
+            for object_ind , class_id in enumerate(detection_supervision.class_id):
+                if cls_names[class_id] == "goalkeeper":
+                    detection_supervision.class_id[object_ind] = cls_names_inv["player"]
+
+            print(detection_supervision)
+
+            break
