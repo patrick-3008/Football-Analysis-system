e8beabbfe3ff9727b991d63891b7661b8b38dce9
Patrick <patricknaashat@yahoo.com>
2024-08-09 12:08:09 +0300

    test read video and output video functions

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+from utils import read_video, save_video
+
 def main():
-    print('Hello')
+    # Read Video
+    video_frames = read_video('input_videos/08fd33_4.mp4')
+
+    # Save Video
+    save_video(video_frames, 'output_videos/output_video.avi')
 
 if __name__ == '__main__':
     main()
