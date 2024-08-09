b5cc9cf92fe425897d7191f75e1e58a15979083e
Patrick <patricknaashat@yahoo.com>
2024-08-09 11:59:29 +0300

    add save video function

--- utils/video_utils.py
+++ utils/video_utils.py
@@ -9,3 +9,10 @@ def read_video(video_path):
             break
         frames.append(frame)
     return frames
+
+def save_video(ouput_video_frames,output_video_path):
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter(output_video_path, fourcc, 24, (ouput_video_frames[0].shape[1], ouput_video_frames[0].shape[0]))
+    for frame in ouput_video_frames:
+        out.write(frame)
+    out.release()
