97d92fec95986a5e9abdc2e1553878e83abf017c
Patrick <patricknaashat@yahoo.com>
2024-09-26 12:27:04 +0300

    cluster image into 2 clusters

--- main.py
+++ main.py
@@ -12,19 +12,6 @@ def main():
                                         read_from_stub=True,
                                         stub_path="stubs/track_stubs.pkl")
     
-    # Save cropped image of a player
-    for track_id, player in tracks['players'][0].items():
-        bbox = player['bbox']
-        frame = video_frames[0]
-
-        # crop bbox from frame
-        cropped_image = frame[int(bbox[1]):int(bbox[3]), int(bbox[0]):int(bbox[2])]
-
-        # save the cropped image
-        cv2.imwrite(f'output_videos/cropped_img.jpg', cropped_image)
-
-        break
-    
     # Draw output
     output_video_frames = tracker.draw_annotations(video_frames, tracks)
 
